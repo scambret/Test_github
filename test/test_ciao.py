080dd47fb6016d0ce021f000a246fa021a93b66c
scambret <66075950+scambret@users.noreply.github.com>
2022-10-30 09:11:56 +0000

    Update test_ciao.py

--- test/test_ciao.py
+++ test/test_ciao.py
@@ -1,1 +1,10 @@
+import pytest
 
+def let_s_test_it(x: int) -> int:
+  return x
+
+def test_intially():
+  x = 2
+  assert isinstance(x, int)
+  assert let_s_test_it(x) == 2
+  assert isinstance(let_s_test_it(x), int)
