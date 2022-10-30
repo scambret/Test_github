4c17d797210a38e34ee64444f263a55019e1ef93
scambret <66075950+scambret@users.noreply.github.com>
2022-10-30 11:56:15 +0000

    Update all_test.py

--- new_folder/test/all_test.py
+++ new_folder/test/all_test.py
@@ -1,1 +1,10 @@
+import pytest
+from new_folder.code import change
 
+def test_all():
+  x = 2
+  assert x == 2
+  assert isinstance(x, int)
+  y = change(x)
+  assert y == 4
+  assert isinstance(y, int)
